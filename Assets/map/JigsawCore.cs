6255ca3cb9b856ffcb2404ea594479c79ca24881
Jill505 <orangec.org@gmail.com>
2024-07-26 13:51:44 +0800

    JigsawSystemUpdateA

---
+++ Assets/map/JigsawCore.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JigsawCore : MonoBehaviour
+{
+    public int Floor = 0;// 0-"外來強盜幫們" 1-"巴別塔強盜幫和結晶人" 2-"蒙特婁vs圖爾瑪" 3-"死靈法師與戰爭勝利者"
+    public int levelCount = 0;
+
+    public List<GameObject> Floor_0;
+    public List<GameObject> Floor_1;
+    public List<GameObject> Floor_2;
+    public List<GameObject> Floor_3;
+
+    public List<GameObject> BossFloor;
+
+    GameObject JigsawOnLoad;
+
+    public bool[] lastTop = new bool[12];
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (gameReset)
+    }
+
+    public void gameStartReset()
+    {
+        //set Base Jigsaw
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateNewJigsaw()
+    {
+        levelCount++;
+        if (levelCount == 25)//某種條件 載入boss關卡
+        {
+
+        }
+        else
+        {
+            int RandomNum;
+            //generate
+            switch (Floor)
+            {
+                case 0:
+                    GenerateFloor0();
+                    break;
+                
+                case 1:
+                    RandomNum = Random.Range(0, Floor_1.Count);
+                    break;
+
+                case 2:
+                    RandomNum = Random.Range(0, Floor_2.Count);
+                    break;
+
+                case 3:
+                    RandomNum = Random.Range(0, Floor_3.Count);
+                    break;
+            }
+        }
+    }
+
+    public void InsJigsaw()
+    {
+        Instantiate(JigsawOnLoad, new Vector3(0,0,0), Quaternion.identity);
+    }
+
+    public void GenerateFloor0()
+    {
+        int RandomNum;
+        RandomNum = Random.Range(0, Floor_0.Count);
+
+        bool succ =false;
+
+        bool[] compare = Floor_0[RandomNum].GetComponent<JigsawCornerStone>().button;
+
+        for (int i = 0; i < 12; i++)
+        {
+            if (lastTop[i] == compare[i] == true)
+            {
+                //執行生成
+                succ = true;
+                break;
+            }
+        }
+
+        if (succ)
+        {
+
+        }
+        else
+        {
+            //re generate
+            GenerateFloor0();
+        }
+    }
+}
---
+++ Assets/map/JigsawCornerStone.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JigsawCornerStone : MonoBehaviour
+{
+    public bool[] top = new bool[12];
+    public bool[] button = new bool[12];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
